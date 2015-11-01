3cd60138e5a3c27c23b80d1ed5f640de137c759b
Alexandre Hamez <alexandre.hamez+github@gmail.com>
2015-11-01 14:41:15 +0100

    Add formula for gf-complete

---
+++ gf-complete.rb
@@ -0,0 +1,23 @@
+class GfComplete < Formula
+  desc ""
+  homepage ""
+  head "http://lab.jerasure.org/jerasure/gf-complete.git", :using => :git
+  version ""
+  sha256 ""
+
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "libtool"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/gf_add", "1", "2", "32"
+  end
+end
