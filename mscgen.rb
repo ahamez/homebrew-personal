d9c905489b4d04da8e6bc4734bf22ff86cd01e44
Alexandre Hamez <alexandre.hamez+github@gmail.com>
2015-11-17 10:56:21 +0100

    Remove spurious test

--- mscgen.rb
+++ mscgen.rb
@@ -5,9 +5,9 @@ class Mscgen < Formula
   version "0.20"
   sha256 "3c3481ae0599e1c2d30b7ed54ab45249127533ab2f20e768a0ae58d8551ddc23"
 
-  depends_on "autoconf"
-  depends_on "automake"
-  depends_on "libtool"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "libgd"
 
   def install
@@ -15,9 +15,5 @@ class Mscgen < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
-  end
-
-  test do
-    system "#{bin}/gf_add", "1", "2", "32"
   end
 end
